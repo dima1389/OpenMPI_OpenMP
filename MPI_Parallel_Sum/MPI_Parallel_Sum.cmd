3b0591425f348acf0e711fd0a8779a7f528be134
dima1389 <dimitrije.cuk@gmail.com>
2025-12-12 00:32:40 +0100

    Add MPI Parallel Sum implementation and update related tasks

--- MPI_Parallel_Sum/MPI_Parallel_Sum.cmd
+++ MPI_Parallel_Sum/MPI_Parallel_Sum.cmd
@@ -11,13 +11,13 @@ set "MSMPI_LIB64=C:\Program Files (x86)\Microsoft SDKs\MPI\Lib\x64"
 cd /d %~dp0
 
 rem --- Build MPI program with MinGW g++ + MSMPI ---
-echo Building MPI_Bcast_And_Reduce...
-g++ MPI_Bcast_And_Reduce.c ^
+echo Building MPI_Parallel_Sum...
+g++ MPI_Parallel_Sum.c ^
   -I"%MSMPI_INC%" ^
   -L"%MSMPI_LIB64%" ^
   -lmsmpi ^
-  -o MPI_Bcast_And_Reduce.exe
+  -o MPI_Parallel_Sum.exe
 
-call mpiexec -n 4 MPI_Bcast_And_Reduce.exe
+call mpiexec -n 4 MPI_Parallel_Sum.exe
 
 endlocal
